f96973662657bf84df147bf14f8b9fef5a1c449a
Ivan Wolf <iwolf@detailag.ch>
2018-12-27 15:59:50 +0100

    Began modernising module

--- config/module.config.php
+++ config/module.config.php
@@ -1,37 +1,37 @@
 <?php
 
-return array(
-    'service_manager' => array(
-        'abstract_factories' => array(
-        ),
-        'aliases' => array(
-        ),
-        'invokables' => array(
-        ),
-        'factories' => array(
+return [
+    'service_manager' => [
+        'abstract_factories' => [
+        ],
+        'aliases' => [
+        ],
+        'invokables' => [
+        ],
+        'factories' => [
             'Detail\Notification\Notifier'              => 'Detail\Notification\Factory\NotifierFactory',
             'Detail\Notification\SenderManager'         => 'Detail\Notification\Factory\SenderManagerFactory',
             'Detail\Notification\Options\ModuleOptions' => 'Detail\Notification\Factory\Options\ModuleOptionsFactory',
-        ),
-        'initializers' => array(
+        ],
+        'initializers' => [
             'Detail\Notification\Service\NotifierInitializer',
-        ),
-        'shared' => array(
-        ),
-    ),
-    'controllers' => array(
-        'initializers' => array(
+        ],
+        'shared' => [
+        ],
+    ],
+    'controllers' => [
+        'initializers' => [
             'Detail\Notification\Service\NotifierInitializer',
-        ),
-    ),
-    'detail_notification' => array(
-        'sender_factories' => array(
+        ],
+    ],
+    'detail_notification' => [
+        'sender_factories' => [
             'webhook' => 'Detail\Notification\Factory\Sender\WebhookSenderFactory',
-        ),
-        'senders' => array(
-            'webhook' => array(
-                'user_agent' => 'dfw-notification/0.1.0',
-            ),
-        ),
-    ),
-);
+        ],
+        'senders' => [
+            'webhook' => [
+                'user_agent' => 'dfw-notification/1.0.0',
+            ],
+        ],
+    ],
+];
